4886765d9cf2a7109cbe3456ea94136c8deb4197
pabloosorio <pablo.osorio@xmarts.com>
2018-05-19 11:38:02 -0500

    Series en factura Unicornio

---
+++ __openerp__.py
@@ -0,0 +1,10 @@
+{
+'name': "Series en Factura UNICORNIO",
+'description' : "Agrega campo serie",
+'version' : "1.0.0",
+'author' : "Pablo Osorio",
+'depends' : ['account_accountant','sale','stock'],
+'data': [],
+'installable' : True,
+}
+
